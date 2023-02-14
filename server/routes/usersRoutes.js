e7e348e5f7d3231e2bd030e3a770603be2d094c3
Abdulrahman Bahyan <d7mii.b@gmail.com>
2023-02-14 19:11:06 +0300

    create update hall interface and API

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -5,11 +5,11 @@ const requireAuth = require('../middleware/requireAuth');
 const router = express.Router();
 
 router.post('/login', usersControllers.login);
-
+router.post('/addUsers', usersControllers.addUser);
 router.use(requireAuth);
 
 router.get('/', usersControllers.getUsers);
-router.post('/addUsers', usersControllers.addUser);
+
 router.get('/getUsersType', usersControllers.getUsersTypes);
 router.delete('/deleteUser', usersControllers.deleteUser);
 router.put('/updateUser', usersControllers.updateUser);
