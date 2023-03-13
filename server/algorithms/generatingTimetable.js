2f7ebfac854dce567ec179d6e7478dce344d511b
Abdulrahman Bahyan <d7mii.b@gmail.com>
2023-03-13 09:14:01 +0300

    Create getNeighbors function

--- server/algorithms/generatingTimetable.js
+++ server/algorithms/generatingTimetable.js
@@ -28,7 +28,15 @@ module.exports.generatingTimetable = async (req, res) => {
 
         let bestTimetable = 0;
         let candidateTimetable = await initialTimetable(modules, groups, halls, days, times);
-        let tabuList = 0;
+        let tabuList = [];
+
+        let i = 0;
+
+        while (i < 2) {
+            const neighborhood = getNeighbors(candidateTimetable, modules, groups, halls, days, times);
+            candidateTimetable = neighborhood[0];
+            i++;
+        }
 
         return res.status(200).json(candidateTimetable);
     }
@@ -110,9 +118,67 @@ const initialTimetable = (modules, groups, halls, days, times,) => {
                     const endTime = (times[time.Start_Time, ((time.Time_ID - 1) + m.Credit_Tutorial) - 1] && times[time.Start_Time, ((time.Time_ID - 1) + m.Credit_Tutorial) - 1].End_Time);
                     e.End_Time = endTime;
                 }
+
+                if (e.Start_Time === '3') {
+                    e.End_Time = '5';
+                }
             }
         })
     })
 
     return timetable;
+}
+
+const getNeighbors = (candidateTimetable, modules, groups, halls, days, times) => {
+    let neighbors = [];
+    var timetable = JSON.parse(JSON.stringify(candidateTimetable));
+
+    while (neighbors.length < 2) {
+        timetable.forEach(e => {
+            const newDay = getRandomItem(days);
+            e.Day_ID = newDay.Day_ID;
+
+            const newTime = getRandomItem(times);
+            e.Start_Time = newTime.Start_Time;
+
+            groups.forEach(g => {
+                modules.forEach(async m => {
+                    if (e.Group_ID === g.Group_ID && e.Module_ID === m.Module_ID) {
+                        const hallsAvailable = halls.filter(h => {
+                            return h.Hall_Capacity >= g.Group_Count && h.Hall_Type_ID === m.Hall_Type_ID;
+                        })
+    
+                        const hall = getRandomItem(hallsAvailable);
+    
+                        if (hall) {
+                            e.Hall_ID = hall.Hall_ID;
+                        }
+                    }
+
+                    if (m.Subject_Type_ID === 1) {
+                        const endTime = (times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Theoretical) - 1] && times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Theoretical) - 1].End_Time);
+                        e.End_Time = endTime;
+                    }
+                    else if (m.Subject_Type_ID === 2) {
+                        const endTime = (times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Practical) - 1] && times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Practical) - 1].End_Time);
+                        e.End_Time = endTime;
+                    }
+                    else if (m.Subject_Type_ID === 3) {
+                        const endTime = (times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Tutorial) - 1] && times[newTime.Start_Time, ((newTime.Time_ID - 1) + m.Credit_Tutorial) - 1].End_Time);
+                        e.End_Time = endTime;
+                    }
+                    
+                    if (e.Start_Time === '3') {
+                        e.End_Time = '5';
+                    }
+
+                })
+            })
+
+        })
+
+        neighbors.push(JSON.parse(JSON.stringify(timetable)));
+    }
+
+    return neighbors;
 }
