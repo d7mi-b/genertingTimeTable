6d446b658f0595b0489bdcd6298d4496b960f07e
Mar3e <mar3e.1420.1435@gmail.com>
2023-03-15 11:11:19 +0300

    add isoverlapping function and changed feasible function

--- server/algorithms/generatingTimetable.js
+++ server/algorithms/generatingTimetable.js
@@ -55,6 +55,13 @@ function getRandomItem(arr) {
 
     return item;
 }
+//This function check is to time sets are overlapping
+function isoverlapping (StartA,EndA,StartB,endB){
+    if(!(EndA <= StartB || StartA >= endB))
+        return true;
+    return false;
+}
+
 
 const initialTimetable = (modules, groups, halls, days, times,) => {
     let timetable = [];
@@ -193,15 +200,15 @@ const feasible = (timetable) => {
         for(var j = i+1 ; j< timetable.length; j++){
             
             // if the same lecturer is assigned to different subjects in same day at same time it's a conflect
-            if(timetable[i].Lecturer_ID === timetable[j].Lecturer_ID && timetable[i].Start_Time === timetable[j].Start_Time && timetable[i].Day_ID === timetable[j].Day_ID)
+            if(timetable[i].Lecturer_ID === timetable[j].Lecturer_ID && timetable[i].Day_ID === timetable[j].Day_ID && isoverlapping(timetable[i].Start_Time,timetable[i].End_Time,timetable[j].Start_Time,timetable[i].End_Time))
             return false
 
             // if the same group is assigned to two lecturs in same day at same time it's a conflect
-            if(timetable[i].Group_ID === timetable[j].Group_ID && timetable[i].Start_Time === timetable[j].Start_Time && timetable[i].Day_ID === timetable[j].Day_ID)
+            if(timetable[i].Group_ID === timetable[j].Group_ID && timetable[i].Day_ID === timetable[j].Day_ID && isoverlapping(timetable[i].Start_Time,timetable[i].End_Time,timetable[j].Start_Time,timetable[i].End_Time))
             return false
 
              // if the same hall is assigned to two groups in same day at same time it's a conflect
-             if(timetable[i].Hall_ID === timetable[j].Hall_ID && timetable[i].Start_Time === timetable[j].Start_Time && timetable[i].Day_ID === timetable[j].Day_ID)
+             if(timetable[i].Hall_ID === timetable[j].Hall_ID && timetable[i].Day_ID === timetable[j].Day_ID&& isoverlapping(timetable[i].Start_Time,timetable[i].End_Time,timetable[j].Start_Time,timetable[i].End_Time))
              return false
         }
     }
