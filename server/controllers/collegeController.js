459936a6d55017fc44050c71c8b212fd1881101f
Abdulrahman Bahyan <d7mii.b@gmail.com>
2023-07-03 13:45:40 +0300

    repete partical  module in intialTimetable function

--- server/controllers/collegeController.js
+++ server/controllers/collegeController.js
@@ -1,9 +1,65 @@
 const db = require('../DB');
 
-module.exports.getCollege = async (req, res) => {
+module.exports.getColleges = async (req, res) => {
     try {
-        const [ row ] = await db.query('select * from college')
-        return res.status(200).json(row)
+        const [ colleges ] = await db.query('select * from college')
+        return res.status(200).json(colleges)
+    }
+    catch(err) {
+        return res.status(400).json({err: err.message})
+    }
+}
+
+module.exports.getCollege = async (req, res) => {
+    const { College_ID } = req.params;
+
+    try {
+        const [ college ] = await db.query('select * from college where College_ID = ?', [College_ID])
+
+        return res.status(200).json(college[0])
+    }
+    catch(err) {
+        return res.status(400).json({err: err.message})
+    }
+}
+
+module.exports.addCollege = async (req, res) => {
+    const { College_Name } = req.body;
+
+    try {
+        const [ college ] = await db.query('insert into college (College_Name) values (?) ', [College_Name])
+
+        return res.status(200).json(college)
+    }
+    catch(err) {
+        return res.status(400).json({err: err.message})
+    }
+}
+
+module.exports.updateCollege = async (req, res) => {
+    const { College_ID, College_Name } = req.body;
+
+    try {
+        const [ college ] = await db.query(`
+            update college set College_Name = ? where College_ID = ? 
+        `, [College_Name, College_ID])
+
+        return res.status(200).json(college)
+    }
+    catch(err) {
+        return res.status(400).json({err: err.message})
+    }
+}
+
+module.exports.deleteCollege = async (req, res) => {
+    const { College_ID } = req.body;
+
+    try {
+        const [ college ] = await db.query(`
+            delete from college where College_ID = ? 
+        `, [College_ID])
+
+        return res.status(200).json(college)
     }
     catch(err) {
         return res.status(400).json({err: err.message})
