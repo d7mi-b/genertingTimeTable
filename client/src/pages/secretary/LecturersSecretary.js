2b56401cf25abc18eb9a26879e0590d1ae3c92d6
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-07-04 05:49:45 +0300

    git rid of old files
    and create new one with small fixes in user interface and the names of the pages

--- client/src/pages/secretary/LecturersSecretary.js
+++ client/src/pages/secretary/LecturersSecretary.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import useFetch from "../../hooks/useFetch";
 import { useEffect, useState } from "react";
-import style from "../styles/secretary/secretaryLecturers.module.css";
+import style from "../styles/secretary/lecturers_secretary.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useFetchPost } from "../../hooks/useFetchPost";
@@ -79,143 +79,158 @@ const LecturersSecretary = () => {
   });
 
   return (
-    <div className={style.lectureorsContainer}>
-      <nav className={style.navLink}>
-        <div className={style.navSection}>
-          <div className={style.links}>
-            <Link>السكرتير</Link>
-            <p>&gt;&gt;</p>
-            <Link>الصفحة الرئيسي</Link>
-          </div>
-        </div>
-        <button className={style.navButton} id="addLecturer">
+    <>
+      <div className={style.topBar}>
+        <p>
+          <span>أعضاء</span> هيئة التدريس
+        </p>
+        <button className={style.addButton} id="addLecturer">
           إضافة مدرس
         </button>
-      </nav>
-
-      <section className={style.lecturersContainer}>
-        {lectureorData && (
-          <div>
-            {lectureorData.map((lecturer, index) => (
-              <div className={style.lectureor} key={index}>
-                <div className={style.infoSection}>
-                  <p className={style.name}>
-                    الأستاذ/ة: {lecturer.Lecturer_Name}
-                  </p>
-                  <p>{lecturer.Department_Name}</p>
-                  <div className={style.lecturerAvailabel}>
-                    <label className={style.labels}>
-                      <input
-                        type={"checkbox"}
-                        className={style.checkBox}
-                        checked={lecturer.Not_Available > 0 ? true : false}
-                        onChange={() => {}}
-                      ></input>{" "}
-                      غير متاح
-                    </label>
-                  </div>
-                </div>
-                <hr className={style.sectionHR} />
-                <div className={style.daySection}>
-                  <div className={style.availabelDays}>
-                    <p>عدد أيام الحضور: </p>
-                    <input
-                      className={style.daysInput}
-                      type="number"
-                      value={lecturer.NO_Available_Days || 0}
-                      readOnly={true}
-                    ></input>
-                  </div>
-                  <div className="daysCheckBoxes">
-                    <div className="checkboxTitle">
-                      <p>الأيام المتاحة: </p>
-                    </div>
-                    <div className={style.checkboxDays}>
-                      {days.map((element, index) => (
-                        <div className="days" key={index}>
-                          <label className={style.labels}>
-                            <input
-                              type="checkbox"
-                              className={style.checkBox}
-                              checked={
-                                lecturer[`${element.value}`] > 0 ? true : false
-                              }
-                              onChange={() => {}}
-                            ></input>{" "}
-                            {element.day}
-                          </label>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </section>
-
-      <section className={style.addLecturerContainer} id="addLecturerContainer">
-        <div className={style.inputCard}>
-          <form onSubmit={handleSubmit}>
-            <div className="card">
-              <div className={style.cardHeader}>
-                <p className={style.title}> إضافة عضو هيئة تدرس</p>
-                <FontAwesomeIcon
-                  icon={faXmark}
-                  className={style.closeIcon}
-                  id="closeIcon"
-                ></FontAwesomeIcon>
-              </div>
-              <div className={style.cardContent}>
-                <label htmlFor="name">الإسم:</label>
-                <section className={style.input}>
-                  <FontAwesomeIcon icon={faUser} className={style.icon} />
-                  <input
-                    type="text"
-                    name="name"
-                    required
-                    value={lecturerName}
-                    onChange={(e) => setName(e.target.value)}
-                    className={style.inputFeild}
-                  />
-                </section>
-                <label htmlFor="academicRefrence">الرتبة العلمية:</label>
-                <section className={style.referenceChoices}>
-                  <section className={style.radioSection}>
-                    <label htmlFor="academicReference">أستاذ/ة</label>
-                    <input
-                      required
-                      className={style.radio}
-                      type="radio"
-                      value={"teacher"}
-                      onClick={() => setRefrence("teacher")}
-                      name="academicReference"
-                    ></input>
-                  </section>
-                  <section className={style.radioSection}>
-                    <label htmlFor="academicReference">دكتور/ة</label>
-                    <input
-                      required
-                      className={style.radio}
-                      type="radio"
-                      value={"doctor"}
-                      onClick={() => setRefrence("doctor")}
-                      name="academicReference"
-                    ></input>
-                  </section>
-                </section>
-                <button className={style.addButton} id="submitLecturer">
-                  إضافة
-                </button>
-              </div>
-            </div>
-          </form>
+      </div>
+      <div className={style.searchBar}>
+        <p>عدد المدرسين</p>
+        <div className={style.sort}>
+          <p>الترتيب حسب</p>
+          <select>
+            <option value="ddd">ddd</option>
+            <option value="ddd">ddd</option>
+            <option value="ddd">ddd</option>
+            <option value="ddd">ddd</option>
+          </select>
         </div>
-      </section>
-      <Done result={result} />
-      <Failed error={error} />
-    </div>
+        <input type="text" />
+      </div>
+      <div className={style.page}>
+        <section className={style.lectureorsContainer}>
+          {lectureorData && (
+            <div>
+              {lectureorData.map((lecturer, index) => (
+                <div className={style.lectureor} key={index}>
+                  <div className={style.infoSection}>
+                    <p className={style.name}>
+                      الأستاذ/ة: {lecturer.Lecturer_Name}
+                    </p>
+                    <p>{lecturer.Department_Name}</p>
+                    <div className={style.lecturerAvailabel}>
+                      <label className={style.labels}>
+                        <input
+                          type={"checkbox"}
+                          className={style.checkBox}
+                          checked={lecturer.Not_Available > 0 ? true : false}
+                          onChange={() => {}}
+                        ></input>{" "}
+                        غير متاح
+                      </label>
+                    </div>
+                  </div>
+                  <hr className={style.sectionHR} />
+                  <div className={style.daySection}>
+                    <div className={style.availabelDays}>
+                      <p>عدد أيام الحضور: </p>
+                      <input
+                        className={style.daysInput}
+                        type="number"
+                        value={lecturer.NO_Available_Days || 0}
+                        readOnly={true}
+                      ></input>
+                    </div>
+                    <div className="daysCheckBoxes">
+                      <div className="checkboxTitle">
+                        <p>الأيام المتاحة: </p>
+                      </div>
+                      <div className={style.checkboxDays}>
+                        {days.map((element, index) => (
+                          <div className="days" key={index}>
+                            <label className={style.labels}>
+                              <input
+                                type="checkbox"
+                                className={style.checkBox}
+                                checked={
+                                  lecturer[`${element.value}`] > 0
+                                    ? true
+                                    : false
+                                }
+                                onChange={() => {}}
+                              ></input>{" "}
+                              {element.day}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </section>
+
+        <section
+          className={style.addLecturerContainer}
+          id="addLecturerContainer"
+        >
+          <div className={style.inputCard}>
+            <form onSubmit={handleSubmit}>
+              <div className="card">
+                <div className={style.cardHeader}>
+                  <p className={style.title}> إضافة عضو هيئة تدرس</p>
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    className={style.closeIcon}
+                    id="closeIcon"
+                  ></FontAwesomeIcon>
+                </div>
+                <div className={style.cardContent}>
+                  <label htmlFor="name">الإسم:</label>
+                  <section className={style.input}>
+                    <FontAwesomeIcon icon={faUser} className={style.icon} />
+                    <input
+                      type="text"
+                      name="name"
+                      required
+                      value={lecturerName}
+                      onChange={(e) => setName(e.target.value)}
+                      className={style.inputFeild}
+                    />
+                  </section>
+                  <label htmlFor="academicRefrence">الرتبة العلمية:</label>
+                  <section className={style.referenceChoices}>
+                    <section className={style.radioSection}>
+                      <label htmlFor="academicReference">أستاذ/ة</label>
+                      <input
+                        required
+                        className={style.radio}
+                        type="radio"
+                        value={"teacher"}
+                        onClick={() => setRefrence("teacher")}
+                        name="academicReference"
+                      ></input>
+                    </section>
+                    <section className={style.radioSection}>
+                      <label htmlFor="academicReference">دكتور/ة</label>
+                      <input
+                        required
+                        className={style.radio}
+                        type="radio"
+                        value={"doctor"}
+                        onClick={() => setRefrence("doctor")}
+                        name="academicReference"
+                      ></input>
+                    </section>
+                  </section>
+                  <button className={style.addButton} id="submitLecturer">
+                    إضافة
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </section>
+        <Done result={result} />
+        <Failed error={error} />
+      </div>
+    </>
   );
 };
 
